d8ceabac38435ae9289be71fa08375e7ea21437d
CyrilHermann <cyrilhermanntrash@gmail.com>
2026-07-12 08:40:36 +0200

    Create lists.js

---
+++ lists.js
@@ -0,0 +1,19 @@
+window.OTJV_LISTS = {
+  coachedPeople: [
+    "Jean Dupont",
+    "Marie Martin",
+    "Pierre Durand",
+    "Sophie Bernard",
+  ],
+
+  maintenanceActivities: [
+    "Maintenance préventive",
+    "Maintenance corrective",
+    "Diagnostic de panne",
+    "Contrôle technique",
+    "Réglage machine",
+    "Remplacement de pièce",
+    "Nettoyage technique",
+    "Inspection de sécurité",
+  ],
+};
