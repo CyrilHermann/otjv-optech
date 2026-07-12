8fca27e28644b17117fd99f6b0ac896185a7192b
CyrilHermann <cyrilhermanntrash@gmail.com>
2026-07-12 08:58:56 +0200

    Update lists.js

--- lists.js
+++ lists.js
@@ -1,19 +1,185 @@
 window.OTJV_LISTS = {
-  coachedPeople: [
-    "Jean Dupont",
-    "Marie Martin",
-    "Pierre Durand",
-    "Sophie Bernard",
+  levels: [
+    {
+      value: "Basic",
+      label: "Basic (1)",
+      numericValue: 1,
+    },
+    {
+      value: "Intermediate",
+      label: "Intermediate (2)",
+      numericValue: 2,
+    },
   ],
 
-  maintenanceActivities: [
-    "Maintenance préventive",
-    "Maintenance corrective",
-    "Diagnostic de panne",
-    "Contrôle technique",
-    "Réglage machine",
-    "Remplacement de pièce",
-    "Nettoyage technique",
-    "Inspection de sécurité",
+  coachedPeople: [
+    "Chaïm Monfort",
+    "Giacomo Nocerino",
+    "Hasan Duraku",
+    "Karen Piroyan",
+  ],
+
+  activities: [
+    {
+      level: "Basic",
+      name: "MV - Accès local chimique",
+    },
+    {
+      level: "Basic",
+      name: "MV - Atelier - introduction Poste de travail",
+    },
+    {
+      level: "Basic",
+      name: "MV - Changement de format Etuieuse/encartonneuse",
+    },
+    {
+      level: "Basic",
+      name: "MV - HHS/TESLA/Robatech - Arrêt de la chauffe de la colle",
+    },
+    {
+      level: "Basic",
+      name: "MV - IMA - Déverrouiller frein (CTRL jeu + méca)",
+    },
+    {
+      level: "Basic",
+      name: "MV - IMA - Nettoyage tuyau Vaccuostat",
+    },
+    {
+      level: "Basic",
+      name: "MV - IMA - Remplacement tuyaux + raccords",
+    },
+    {
+      level: "Basic",
+      name: "MV - IMA - TK504 - Soufflage Secteur vide Roue",
+    },
+    {
+      level: "Basic",
+      name: "MV - IMA - TK505 - Changer Ventouses",
+    },
+    {
+      level: "Basic",
+      name: "MV - IMA - TK505 - Contrôle Gabarit",
+    },
+    {
+      level: "Basic",
+      name: "MV - IMA - TK505 - Etuvage Ventouses",
+    },
+    {
+      level: "Basic",
+      name: "MV - IMA - TK521 - LUB pinces",
+    },
+    {
+      level: "Basic",
+      name: "MV - IMA - TK549 - chgt tuyau blanc vaccum principal",
+    },
+    {
+      level: "Basic",
+      name: "MV - LUB - pompe à graisse à levier + chgt cartouche",
+    },
+    {
+      level: "Basic",
+      name: "MV - LUB - Spray",
+    },
+    {
+      level: "Basic",
+      name: "MV - MEURER - Nettoyage tête Markem",
+    },
+    {
+      level: "Basic",
+      name: "MV - OPTIMA - changer un venturi",
+    },
+    {
+      level: "Basic",
+      name: "MV - OPTIMA - Mesure de vide",
+    },
+    {
+      level: "Basic",
+      name: "MV - OPTIMA - Remplacement Joint plaque presseur",
+    },
+    {
+      level: "Basic",
+      name: "MV - Outil - Maintenance Aspi DC5900",
+    },
+    {
+      level: "Basic",
+      name: "MV - Outil - Tensiomètre - mesure (OPTIMA lift up ou sup)",
+    },
+    {
+      level: "Basic",
+      name: "MV - Outil - Tournevis Dynamo - serrage au couple",
+    },
+    {
+      level: "Basic",
+      name: "MV - Outil - utilisation DMO Logsheet + PM03 OPTech",
+    },
+    {
+      level: "Basic",
+      name: "MV - SCHUBERT - CTRL alignement retourneur transmodule",
+    },
+
+    {
+      level: "Intermediate",
+      name: "MV - Chgt Manomètre",
+    },
+    {
+      level: "Intermediate",
+      name: "MV - IMA - chgt Aspi Markem",
+    },
+    {
+      level: "Intermediate",
+      name: "MV - IMA - TK506 - Démontage Roue",
+    },
+    {
+      level: "Intermediate",
+      name: "MV - IMA - TK511 - chgt bille blanche",
+    },
+    {
+      level: "Intermediate",
+      name: "MV - IMA - TK511 - chgt pince plaque noire",
+    },
+    {
+      level: "Intermediate",
+      name: "MV - IMA - TK548 - ouverture/fermeture pinces",
+    },
+    {
+      level: "Intermediate",
+      name: "MV - IMA/MEURER - Purge bac colle + nettoyage sonde bac colle",
+    },
+    {
+      level: "Intermediate",
+      name: "MV - OPTIMA - chgt plaque transport",
+    },
+    {
+      level: "Intermediate",
+      name: "MV - OPTIMA - chgt pipe scellage",
+    },
+    {
+      level: "Intermediate",
+      name: "MV - OPTIMA - chgt tête de scellage",
+    },
+    {
+      level: "Intermediate",
+      name: "MV - OPTIMA - chgt télécommande doseur complet",
+    },
+    {
+      level: "Intermediate",
+      name: "MV - OPTIMA - chgt tuyaux lift up",
+    },
+    {
+      level: "Intermediate",
+      name: "MV - OPTIMA - CTRL redémarrage fast stop",
+    },
+    {
+      level: "Intermediate",
+      name: "MV - OPTIMA - Faire avancer les chaînes en manuel",
+    },
+    {
+      level: "Intermediate",
+      name: "MV - Process - Chgt Manchette",
+    },
+    {
+      level: "Intermediate",
+      name: "MV - Process - Nettoyage des conduites de silo de tempérage",
+    },
   ],
 };
